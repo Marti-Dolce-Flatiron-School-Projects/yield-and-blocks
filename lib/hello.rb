5b7252e7c2eb3b22db3641a5f3ff053c1789abe0
Martinique <me@martidolce.com>
2020-11-13 23:55:44 -0500

    commit tree cf4a231b0481f875813f056629d8136894c476ca

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,25 @@
-def hello_t
+# frozen_string_literal: true
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+
+    array
+  else
+    puts 'Hey! No block was given!'
+  end
 end
 
 # call your method here!
 
+%w[Tim Tom Jim].each do |name|
+  puts "Hi, #{name}" if name.start_with?('T')
+end
